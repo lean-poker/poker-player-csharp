7ebe29f849f255c3fdf76fbfac3ae1a0aa33a77e
Bence Kiglics <bence.kiglics@gmail.com>
2018-01-28 08:40:13 +0100

    Parsing JSON to a structure

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -6,13 +6,13 @@ namespace Nancy.Simple
 	{
 		public static readonly string VERSION = "Default C# folding player";
 
-		public static int BetRequest(JObject gameState)
+		public static int BetRequest(Game gameState)
 		{
 			//TODO: Use this method to return the value You want to bet
 			return 0;
 		}
 
-		public static void ShowDown(JObject gameState)
+		public static void ShowDown(Game gameState)
 		{
 			//TODO: Use this method to showdown
 		}
