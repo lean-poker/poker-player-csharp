4b1eb0eec755e8224e825a1671c17d8d2f9d475e
Bence Kiglics <bence.kiglics@gmail.com>
2018-01-28 09:00:42 +0100

    Added a simple test wuth NUnit

---
+++ src/TestPokerPlayer.cs
@@ -0,0 +1,15 @@
+﻿namespace Nancy.Simple
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestPokerPlayer
+    {
+        [Test]
+        public void TestPokerPlayerFolds()
+        {
+            Assert.AreEqual(0, PokerPlayer.BetRequest(new Game()));
+        }
+    }
+}
